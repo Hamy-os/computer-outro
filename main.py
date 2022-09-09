2e04372411b741f63d81adb04a11a469454a27ac
hamy-os <hamzaalisic0@gmail.com>
2022-09-09 10:48:26 +0300

    feat: new version that doesnt crash your pc

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import subprocess
 import keyboard
 import os
 from pygame import mixer
-from ctypes import windll
-from ctypes import c_int
-from ctypes import c_uint
-from ctypes import c_ulong
-from ctypes import POINTER
-from ctypes import byref
+import subprocess
 
 # Find and play sound (if in the same folder as this file)
 outrosound = os.path.join(os.getcwd(), 'outro.mp3')
@@ -25,24 +20,9 @@ keyboard.press_and_release("f11")
 for zaky in range(10):
     print("Shutting down in: " + str(10 - zaky) + " Seconds")
     time.sleep(1)
-    
 
 print("Shutting down now!")
 time.sleep(0.5)
-nullptr = POINTER(c_int)()
 
-windll.ntdll.RtlAdjustPrivilege(
-    c_uint(19), 
-    c_uint(1), 
-    c_uint(0), 
-    byref(c_int())
-)
-
-windll.ntdll.NtRaiseHardError(
-    c_ulong(0xC000007B), 
-    c_ulong(0), 
-    nullptr, 
-    nullptr, 
-    c_uint(6), 
-    byref(c_uint())
-)
+bat_file_path = str(f"{os.path.dirname(__file__)}/bsod.bat")
+subprocess.call([bat_file_path])
